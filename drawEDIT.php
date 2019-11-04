171d8e7206af602c504625354eb6b024c89d21f9
draper21 <57270240+draper21@users.noreply.github.com>
2019-11-04 09:48:46 -0500

    11/4/19 9:45 A.M.

--- drawEDIT.php
+++ drawEDIT.php
@@ -13,27 +13,41 @@ if ($_SESSION["empID"] == "Failed" || is_null($_SESSION["empID"]))
   <div class="container-fluid">
 
 <section class="section-margin" style="min-height: 50vh;">
+  <div class="row justify-content-center">
+    <div class="col-sm-2" align="center">
+      <br>
+      <br>
+      <div class="alert alert-info alert-dismissible">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+            <strong>Start Here!</strong>
+    </div>  
+  </div> 
 
-<div class="container-fluid">
+    <div class="col-sm-4" align="center">
     <div class="media contact-info">
       <span class="contact-info__icon"><i class="lnr lnr-enter"></i></span>
       <div class="media-body">
         <h2>Edit Drawing</h2>
         <p>Welcome<?php echo " " . $_SESSION["empID"] . "!"; ?></p>
       </div>
-      
     </div>
-    <!-- RESET SEARCH VIA DESTROY SESSION VARIABLES -->
-    <button type="button" class="btn btn-warning btn-md" onclick="window.location.href ='drawedit.php';">Reset</button>
-    <br><br>
-    <div class="alert alert-info alert-dismissible" style="width:10%">
-          <button type="button" class="close" data-dismiss="alert">&times;</button>
-            <strong>Start Here!</strong>
-        </div>
-    <div class="row">
+   </div>
 
+    <!--END DIV CLASS ROW JUSTIFY CONTENT CENTER -->
+
+    <!-- RESET SEARCH VIA DESTROY SESSION VARIABLES -->
+    <div class="col-sm-2" align="center">
+    <br>
+    <br>
+    <button type="button" class="btn btn-warning btn-md" onclick="window.location.href ='search.php';">Reset</button>
+   
+    </div>
+    <!-- END RESET BUTTON -->
+       
+    </div>
+    <div class="row justify-content-center">
       <!-- DROPDOWN 1 -->
-      <div class="col">
+      <div class="col-sm-2">
         <form>
           <h4>Department </h4>
           <select name="dropdown" id="dropdown" class="bg-dark text-white" style="width:100%">
@@ -44,7 +58,7 @@ if ($_SESSION["empID"] == "Failed" || is_null($_SESSION["empID"]))
 
 
       <!-- DROPDOWN 2 -->
-      <div class="col">
+      <div class="col-sm-2">
         <form name="dropdown2" method="post">
           <h4>Equipment</h4>
           <select name="dropdown2" id="dropdown2" style="width:100%" class="bg-dark text-white">
@@ -53,9 +67,8 @@ if ($_SESSION["empID"] == "Failed" || is_null($_SESSION["empID"]))
         </form>
       </div>
 
-
       <!-- DROPDOWN 2 SUB -->
-      <div class="col">
+      <div class="col-sm-2">
         <form name="dropdown2sub" method="post">
           <h4>Sub Equip</h4>
           <select name="dropdown2sub" id="dropdown2sub" style="width:100%" class="bg-dark text-white">
@@ -65,7 +78,7 @@ if ($_SESSION["empID"] == "Failed" || is_null($_SESSION["empID"]))
       </div>
 
       <!-- DROPDOWN 3 -->
-      <div class="col">
+      <div class="col-sm-2">
         <form>
           <h4>Filter</h4>
           <input type="text" id="dropdown3" name="dropdown3" class="bg-dark text-white" style="width:100%;" />
@@ -74,36 +87,38 @@ if ($_SESSION["empID"] == "Failed" || is_null($_SESSION["empID"]))
 
     </div>
     <br>
-
-        <div class="row">
-          <table class="table table-striped" id="results">
-            <thead>
-              <tr>
-                <th scope="col">ID</th>
-                <th scope="col">DEPARTMENT</th>
-                <th scope="col">EQUIPMENT</th>
-                <th scope="col">EQ-ID</th>
-                <th scope="col">EQ-SUB</th>
-                <th scope="col">SIZE</th>
-                <th scope="col">NUM</th>
-                <th scope="col">EXT</th>
-                <th scope="col">VENDOR</th>
-                <th scope="col">VEN. DWG</th>
-                <th scope="col">VEN. JOB</th>
-                <th scope="col">REV</th>
-                <th scope="col">DATE</th>
-                <th scope="col">BY</th>
-                <th scope="col">DESCRIPTION</th>
-                <th scope="col">DRAWING</th>
-              </tr>
-            </thead>
-            <tbody id="myTable">
-            </tbody>
-          </table>
-        </div>
+        <div class="row justify-content-center">
+          <div class="col-xl-12">
+            <table class="table table-striped" id="results">
+              <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">DEPARTMENT</th>
+                    <th scope="col">EQUIPMENT</th>
+                    <th scope="col">EQ.ID</th>
+                    <th scope="col">EQ.SUB</th>
+                    <th scope="col">SIZE</th>
+                    <th scope="col">NUM</th>
+                    <th scope="col">EXT</th>
+                    <th scope="col">VENDOR</th>
+                    <th scope="col">VEN.DWG</th>
+                    <th scope="col">VEN.JOB</th>
+                    <th scope="col">REV</th>
+                    <th scope="col">DATE</th>
+                    <th scope="col">BY</th>
+                    <th scope="col">DESCRIPTION</th>
+                    <th scope="col">DRAWING</th>
+                 </tr>
+                </thead>
+               <tbody id="myTable">
+             </tbody>
+           </table>
+         </div>
+       </div>
+       </div>
 
     </section>
-    
+    </div>
     <?php require_once("footer.php");?>
 
     <script>
